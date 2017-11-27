c572087cbf7f257d73f420ba1d3d36bf77939413
Yawei Li <liyawei04@meituan.com>
2017-11-27 13:19:00 +0800

    failed

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var requestHandlers = require("./requestHandlers");
 
 var handle = {}
 handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
-handle["/show"] = requestHandlers.show;
 
 server.start(router.route, handle);
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var http = require("http");
 var url = require("url");
+var serveStatic = require('serve-static');
+var finalhandler = require('finalhandler');
 
 function start(route, handle) {
   function onRequest(request, response) {
+    var serve = serveStatic('public',
+        {
+           'index': ['index.html'],
+           'upload': ['upload.html', 'upload'],
+            'test': ['test.png', 'test.PNG']
+        });
+    serve(request, response, finalhandler(request, response));
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
     route(handle, pathname, response, request);
